a9947a6121e1b87f7660a9202c64220347d9580e
nayekim <nayegraphy@gmail.com>
2025-03-01 03:09:08 +0900

    질문 form 세팅

---
+++ project4/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Question
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['content']
+        labels = {
+            'content': '질문내용'
+        }
--- project4/urls.py
+++ project4/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import base_views
 
+
 app_name = 'project4'
 urlpatterns = [
     path('', base_views.index, name='index')
