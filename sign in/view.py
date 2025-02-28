3502ec7ae1b29532c5e3cac271cf27dcd0044f35
seojinkim <azaz5679@naver.com>
2025-02-28 16:21:25 +0900

    dummies

---
+++ sign in/view.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        login(request, user)
+        return redirect('home')
+    return render(request, 'signup.html')
