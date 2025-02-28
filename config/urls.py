787f2acf18cd5ff5cf1bfc1c8c92092ebdb73156
nayekim <nayegraphy@gmail.com>
2025-02-28 17:48:42 +0900

    edit settings.py

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
 from project4.views import base_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('project4/', include('project4.urls')),
 
